91ebb7e4796042b649c58299d814a48a204052d4
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-06-30 20:38:06 -0400

    implemented beginning stage of design using interrupts and conditional logic, will implement servo motor to control safe

--- soundDetectorSafe/soundDetectorSafe.ino
+++ soundDetectorSafe/soundDetectorSafe.ino
@@ -1,51 +1,72 @@
 const int DetectorPin = 2;
+const int unlockPin = 10;
+const int resetPin = 4;
 //current Pin: 4s,10s,14s
-//const float expectedTime1 = 4;
-//const float expectedTime2 = 10;
-//const float expectedTime3 = 14;
+volatile byte state1 = LOW;
+volatile byte state2 = LOW;
+volatile byte state3 = LOW;
+bool unlockedState = false;
 float currentTime;
-volatile int bypassNumber = 0;
 bool hearSound;
 bool allowAccess = false;
 void setup() {
+  digitalWrite(resetPin, HIGH);
   currentTime = millis();
   // put your setup code here, to run once:
   pinMode(DetectorPin, INPUT);
+  pinMode(unlockPin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(DetectorPin), sound, LOW);
 }
 
 void loop() {
   digitalRead(DetectorPin);
-  Serial.println((float)millis() / 1000);
-  Serial.print("currentBypass Number ");
-  Serial.println(bypassNumber);
+  Serial.println((float)micros() / 1000000);
+ /* if (state1) {
+    Serial.println("true state 1");
+    Serial.println("");
+  }
+  if (state2) {
+    Serial.println("true state 2");
+    Serial.println("");
+  }
+  if (state3) {
+    Serial.println("true state 3");
+    Serial.println("");
+  }*/
+  if (state1 && state2 && state3) {
+    Serial.println("UNLOCKED");
+    digitalWrite(unlockPin, HIGH);
+    unlockedState = true;
+  };
+
+  Serial.print("state 1: ");
+  Serial.println(state1);
+  Serial.print("state 2: ");
+  Serial.println(state2);
+  Serial.print("state 3: ");
+  Serial.println(state3);
+  
+  if (((float)millis() / 1000) >= 30 && !unlockedState) {
+    digitalWrite(resetPin, LOW);
+  }
   delay(500);
 }
 void sound() {
   Serial.println("SOUND DETECTED");
-  currentTime = (int)micros() / 1000;
-  if (bypassNumber == 0) {
-    if ( 3.5 <= currentTime && currentTime <= 9) {
-      bypassNumber= bypassNumber+1;
-      Serial.println("HITTING IF");
-      Serial.println(bypassNumber);
-    }
-    if (bypassNumber == 1) {
-      if (9.5 <= currentTime && currentTime <= 10.5) {
-        bypassNumber= bypassNumber+1;
-              Serial.println("HITTING IF 2");
-      Serial.println(bypassNumber);
-      } else {
-        bypassNumber = 0;
-      }
-    }
-    if (bypassNumber == 2) {
-      if (13.5 <= currentTime && currentTime <= 14.5) {
-        allowAccess = true;
-      } else {
-        bypassNumber = 0;
-      }
-    }
+  currentTime = (float)micros() / 1000000;
+  
+  if (!state1 && !state2 && !state3 &&  3.5 <= currentTime && currentTime <= 5) {
+    state1 = HIGH;
+    Serial.println("HITTING IF 1");
+  }else if (state1 && !state2 && !state3 && 9.5 <= currentTime && currentTime <= 10.5) {
+    state2 = HIGH;
+    Serial.println("HITTING IF 2");
+  }else if (state1 && state2 && !state3 && 13.5 <= currentTime && currentTime <= 14.5) {
+    Serial.println("HITTING IF 3");
+    state3 = !state3;
+  } else {
+    Serial.println("nothing detected");
   }
 }
